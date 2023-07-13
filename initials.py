2b091a3ec2db336c4125e9e2790263fffce9cb54
Pablo Cortés <cortesoltra@gmail.com>
2023-07-13 16:22:39 +0200

    First two exercises

---
+++ initials.py
@@ -0,0 +1,8 @@
+#Soc Pablo Cortés, Friki casi nivel Programmer
+print('PPPP    CCC')
+print('P   P  C   C')
+print('P   P  C')
+print('PPPP   C')
+print('P      C')
+print('P      C   C')
+print('P       CCC')
