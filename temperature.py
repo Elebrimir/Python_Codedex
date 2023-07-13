ecfaff40eab142e2d67b51c45e0673bdff6e2ac6
Pablo Cortés <cortesoltra@gmail.com>
2023-07-13 17:18:28 +0200

    2 Exercicis més

---
+++ temperature.py
@@ -0,0 +1,11 @@
+# Create a temperature.py program that converts a temperature from Fahrenheit (°F) to Celsius (°C).
+# Google the current temperature of Brooklyn, NY (where Codédex is based) in °F.
+# Use the following formula and write it out in Python:
+# Celsius=1.8(Fahrenheit−32)​
+
+# Print out the converted temperature.
+
+Fahrenheit = 90
+Celsius = (Fahrenheit-32)/1.8
+
+print(Celsius)
