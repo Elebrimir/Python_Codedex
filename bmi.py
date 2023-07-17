b75480f92d372dc5450105a0e28949469237e2aa
Pablo Cortés <cortesoltra@gmail.com>
2023-07-17 22:33:45 +0200

    bmi.py archive

---
+++ bmi.py
@@ -0,0 +1,12 @@
+# The body mass index (BMI) was created by a Belgian mathematician in the 1850s and it's used by health and nutrition professionals to estimate human body fat in certain populations.
+# It is computed by taking an individual's weight (mass) in kilograms and dividing it by the square of their height in meters.
+# bmi=height2mass​
+
+# Create a bmi.py program that calculates your own BMI.
+
+mass = 81
+height = 1.76
+
+bmi = mass / (height**height)
+
+print (bmi)
